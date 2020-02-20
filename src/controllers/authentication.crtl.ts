1972406a1193b074bffa52afc99115e3ca425184
ofirdagan <ofirdagan@gmail.com>
2020-02-20 14:28:24 +0200

    first commit

--- src/controllers/authentication.crtl.ts
+++ src/controllers/authentication.crtl.ts
@@ -1,4 +1,4 @@
-import {APP_ID, BASE_URL} from '../constants';
+import {BASE_URL} from '../constants';
 import express, {Request, Response} from 'express';
 import {getInitialAppTokens} from '../services/authentication.service';
 const router = express.Router();
@@ -6,14 +6,14 @@ const router = express.Router();
 router.get('/oauth-handshake', (req: Request, res: Response) => {
   const token = req.query.token;
   const redirectTo = `${BASE_URL}/authentication/redirect-after-oauth`;
-  const redirectUrl = `https://www.wix.com/app-oauth-installation/consent?token=${token}&appId=${APP_ID}&redirectUrl=${redirectTo}`;
+  const redirectUrl = `https://www.wix.com/app-oauth-installation/consent?token=${token}&appId=${process.env.APP_ID}&redirectUrl=${redirectTo}`;
   res.redirect(redirectUrl);
 });
 
 router.get('/redirect-after-oauth', async (req: Request, res: Response) => {
   const {code, instanceId} = req.query;
   console.log(`app instanceId: ${instanceId}`);
-  const tokens = await getInitialAppTokens(code);
+  const tokens = await getInitialAppTokens(code, process.env.app_secret);
   console.log(`tokens: `, tokens);
   res.redirect('https://www.wix.com/app-oauth-installation/token-received');
 });
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import authenticationController from './controllers/authentication.crtl';
 
 const app = express();
 app.use('/authentication', authenticationController);
 
-const port = 3000;
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Server is up');
+});
+
+const port = process.env.PORT;
+
 console.log(`Listening on port: ${port}`);
 app.listen(port);
