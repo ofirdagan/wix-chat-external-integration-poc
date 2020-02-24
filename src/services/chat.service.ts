6046e5e4a5e631c6f79c50cd6ecd60c07ca5cd17
ofirdagan <ofirdagan@gmail.com>
2020-02-24 09:57:41 +0200

    feat(error handling): add console log

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -1,16 +1,22 @@
 import axios from 'axios';
 
 export const sendMessage = (channelId: string, text: string, accessToken: string) => {
-  axios.post(`https://www.wixapis.com/chat/v1/channels/${channelId}/messages`, {
-    channelId,
-    type: 'TEXT',
-    payload: {
-      text
-    }
-  }, {
-    headers: {
-      authorization: accessToken,
-      'content-type': 'application/json'
-    }
-  });
+  try {
+    axios.post(`https://www.wixapis.com/chat/v1/channels/${channelId}/messages`, {
+      channelId,
+      type: 'TEXT',
+      payload: {
+        text
+      }
+    }, {
+      headers: {
+        authorization: accessToken,
+        'content-type': 'application/json'
+      }
+    });
+  }
+  catch (e) {
+    console.log(`Error sending message ${e}`);
+    throw e;
+  }
 };
